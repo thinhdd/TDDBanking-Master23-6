cc8726bd2615007440405f04326ebfa8981692ca
thinhdd <thinhdd_54@vnu.edu.vn>
2013-06-24 21:53:19 +0700

    test Open Account Green

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -6,11 +6,15 @@
  * To change this template use File | Settings | File Templates.
  */
 public class BankAccount {
+    static BankAccountDAO bankAccountDAO;
     public static void setBankAccountDAO(BankAccountDAO mockDao) {
+        bankAccountDAO = mockDao;
         //To change body of created methods use File | Settings | File Templates.
     }
 
     public static void openAccount(String accountNumber) {
+        BankAccountDTO account = new BankAccountDTO(accountNumber);
+        bankAccountDAO.save(account);
         //To change body of created methods use File | Settings | File Templates.
     }
 }
